fefb5d21a54399738c2083c569801b7059c2d224
dopppler <dopppler@foxmail.com>
2016-02-17 15:20:08 +0800

    添加版权信息，调整目录结构

---
+++ src/jquery.yCalendar.js
@@ -0,0 +1,75 @@
+/*!
+ * Name：yCalendar
+ * Author：Roger shieh
+ * Site：https://github.com/dopppler/plugin-yCalendar
+ * License：MIT
+ */
+
+;(function($) {
+	$.fn.yCalendar = function(options, callback) {
+		
+		// 设置容器的style
+		$(this).css("display","inline-block");
+
+		// 封装HTML
+		var strHTML = '<a class="yCal-btn yCal-btn-minus" href="#">&lt;</a>';
+		strHTML += '<input class="yCal-text" type="text" value="" readonly="readonly">';
+		strHTML += '<a class="yCal-btn yCal-btn-add" href="#">&gt;</a>';
+		$(this).append(strHTML);
+
+		// 封装CSS
+		var strStyle = '<style>'
+		strStyle += '.yCal-text{width:40px;text-align:center;font-size:15px;color:#666;border:none}';
+		strStyle += '.yCal-btn{text-decoration:none;display:inline-block;width:20px;text-align:center;padding:3px;color:#666}';
+		strStyle += '.yCal-btn:hover{color:#fff;background-color:#65aff2}';
+		strStyle += '</style>'
+		$(this).append(strStyle);
+
+		// 获取系统当前年份，用于默认值
+		var now = new Date();
+		var now_year = now.getFullYear();
+
+		// 默认值
+		var defaultVal = {
+			text_year: now_year,
+			min_year: '1978',
+			max_year: '2050'
+		};
+
+		// 接收自定义参数
+		var obj = $.extend(defaultVal, options);
+
+		$(this).find('.yCal-text').val(obj.text_year);
+
+		// 获取当前选择年份
+		var _year = parseFloat($(this).find('.yCal-text').val());
+
+		// 左右按钮事件
+		$(this).find('.yCal-btn-minus').click(function() {
+			if (_year > obj.min_year) {
+				_year--;
+			} else {
+				return false;
+			}
+			$(this).siblings('.yCal-text').val(_year);
+
+			// 添加回调
+			if(callback){
+				callback();
+			}
+		});
+		$(this).find('.yCal-btn-add').click(function() {
+			if (_year < obj.max_year){
+				_year++;	
+			} else {
+				return false;	
+			}
+			$(this).siblings('.yCal-text').val(_year);
+
+			// 添加回调
+			if(callback){
+				callback();
+			}
+		});
+	};
+})(jQuery);
